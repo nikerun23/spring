684ebd8513bd87b7607e9109cc2690ed810ad79b
HK Lee <nikerun23@naver.com>
2017-11-11 18:02:06 +0900

    RedirectAttribute 클래스를 이용한 리다이렉트시 파라미터 전달

--- book_ex/ex01/src/main/java/org/zerock/controller/SearchBoardController.java
+++ book_ex/ex01/src/main/java/org/zerock/controller/SearchBoardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.PageMaker;
 import org.zerock.domain.SearchCriteria;
 import org.zerock.service.BoardService;
@@ -44,5 +45,21 @@ public class SearchBoardController {
 			, Model model)throws Exception {
 		
 		model.addAttribute(service.read(bno));
+	}
+	
+	@RequestMapping(value = "/removePage", method = RequestMethod.GET)
+	public String removePage(@RequestParam("bno") int bno
+			, SearchCriteria cri
+			, RedirectAttributes rttr)throws Exception {
+		
+		service.remove(bno);
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addAttribute("searchType", cri.getSearchType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/sboard/list";
 	}
 }
