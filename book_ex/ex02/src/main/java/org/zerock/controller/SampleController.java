c34c877e244791859df64f9faa5a3daa6ce3e012
HK Lee <nikerun23@naver.com>
2017-11-11 20:09:41 +0900

    jackson 라이브러리를 이용한 JSON 데이터 보내기 테스트

--- book_ex/ex02/src/main/java/org/zerock/controller/SampleController.java
+++ book_ex/ex02/src/main/java/org/zerock/controller/SampleController.java
@@ -1,10 +1,62 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Sample2VO;
 
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
+
+	@RequestMapping(value = "/hello")
+	public String sayHello() {
+		return "hello world !!";
+	}
+	
+	@RequestMapping("/sendVO")
+	public Sample2VO sendVO () {
+		
+		Sample2VO vo = new Sample2VO();
+		vo.setFirstName("Hyunkeun");
+		vo.setLastName("Lee");
+		vo.setMno(123);
+		
+		return vo;
+	}
+	
+	@RequestMapping("/sendList")
+	public List<Sample2VO> sendList() {
+		List<Sample2VO> list = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++) {
+			Sample2VO vo = new Sample2VO();
+			vo.setFirstName("Hyunkeun");
+			vo.setLastName("Lee");
+			vo.setMno(123);
+			list.add(vo);
+		}
+		
+		return list;
+	}
+	
+	@RequestMapping("/sendMap")
+	public Map<Integer, Sample2VO> sendMap() {
+		Map<Integer, Sample2VO> map = new HashMap<>();
+		
+		for (int i = 0; i < 10; i++) {
+			Sample2VO vo = new Sample2VO();
+			vo.setFirstName("Hyunkeun");
+			vo.setLastName("Lee");
+			vo.setMno(123);
+			map.put(i, vo);
+		}
+		
+		return map;
+	}
 
 }
---
+++ book_ex/ex02/src/main/java/org/zerock/domain/Sample2VO.java
@@ -0,0 +1,29 @@
+package org.zerock.domain;
+
+public class Sample2VO {
+
+	private Integer mno;
+	private String firstName;
+	private String lastName;
+	
+	public Integer getMno() {
+		return mno;
+	}
+	public void setMno(Integer mno) {
+		this.mno = mno;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
