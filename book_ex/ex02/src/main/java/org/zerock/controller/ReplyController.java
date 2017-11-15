f807b2b8460017cde2ca3bc8554cc8136388b9a9
HK Lee <nikerun23@naver.com>
2017-11-15 23:18:29 +0900

    REST 클라이언트를 이용하여 RestController 호출

--- book_ex/ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ book_ex/ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,20 +1,50 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 @RestController
-@RequestMapping("/replies/*")
+@RequestMapping("/replies")
 public class ReplyController {
 
 	@Inject
 	private ReplyService service;
 	
-	//@RequestMapping(value = "/all", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<String> register(@RequestBody ReplyVO vo) {
+		
+		ResponseEntity<String> entity = null;
+		try {
+			service.addReply(vo);
+			entity = new ResponseEntity<>("SUCCSS", HttpStatus.OK);
+		} catch(Exception e) {
+			entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	
+	@RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+	public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") Integer bno) {
+		
+		ResponseEntity <List<ReplyVO>> entity = null;
+		try {
+			entity = new ResponseEntity<>(service.listReply(bno), HttpStatus.OK);
+		} catch (Exception e){
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
 	
 	
 }
