a33b8a61d0e35db4e26b99f2fdee006ff16ee95e
HK Lee <nikerun23@naver.com>
2017-11-29 23:33:34 +0900

    Message DAO, mapper 등록

---
+++ book_ex/ex03/src/main/java/org/zerock/persistence/MessageDAOImpl.java
@@ -0,0 +1,25 @@
+package org.zerock.persistence;
+
+import org.zerock.domain.MessageVO;
+
+public class MessageDAOImpl implements MessageDAO {
+
+	@Override
+	public void create(MessageVO vo) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public MessageVO readMessage(Integer mid) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateState(Integer mid) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
