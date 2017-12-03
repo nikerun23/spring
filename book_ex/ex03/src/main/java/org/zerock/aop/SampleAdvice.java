a8311bd74c9e25639f8e016192baabfd145b0ce3
HK Lee <nikerun23@naver.com>
2017-12-04 00:03:15 +0900

    Aspect 클래스 추가

---
+++ book_ex/ex03/src/main/java/org/zerock/aop/SampleAdvice.java
@@ -0,0 +1,23 @@
+package org.zerock.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class SampleAdvice {
+
+	private final Logger logger = LoggerFactory.getLogger(SampleAdvice.class);
+	
+	@Before("execution(* org.zerock.service.MessageService*.*(..))")
+	public void startLog() {
+		
+		logger.info("--------------------------------");
+		logger.info("--------------------------------");
+		
+	}
+	
+}
