367599154ef2e7a94c581458cf1ab90b8e8a649d
HK Lee <nikerun23@naver.com>
2017-12-16 18:55:07 +0900

    업로드 파일 저장 (UUID + originalName)

---
+++ book_ex/ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,56 @@
+package org.zerock.controller;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@Resource (name = "uploadPath")
+	private String uploadPath;
+	
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
+	public void uploadForm() {
+		logger.info("=========== uploadForm ===========");
+	}
+	
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
+	public void uploadForm(MultipartFile file, Model model) throws Exception {
+	
+		logger.info("getOriginalFilename : "+file.getOriginalFilename());
+		logger.info("getSize : "+file.getSize());
+		logger.info("getContentType : "+file.getContentType());
+		logger.info("getBytes : "+file.getBytes());
+		
+		String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		logger.info("savedName : "+savedName);
+		model.addAttribute("savedName", savedName);
+		
+	}
+	
+	private String uploadFile(String originalName, byte[] fileData) throws Exception{
+		
+		UUID uid = UUID.randomUUID();
+		
+		String savedName = uid + "_" + originalName;
+		
+		File target = new File(uploadPath, savedName);
+		
+		FileCopyUtils.copy(fileData, target);
+		
+		return savedName;
+	}
+}
