188286fe8699edfde4774e08e4212dcd239cf47d
HK Lee <nikerun23@naver.com>
2017-12-17 20:07:43 +0900

    Ajax 콘트롤러 작성, RequestMapping(produces)는 한국어를 위하여 설정

--- book_ex/ex04/src/main/java/org/zerock/controller/UploadController.java
+++ book_ex/ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -7,11 +7,14 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -43,6 +46,18 @@ public class UploadController {
 	@RequestMapping(value = "/uploadAjax", method = RequestMethod.GET)
 	public void uploadAjax() {
 		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/uploadAjax", method = RequestMethod.POST
+		, produces = "text/plain;charset=utf-8") // 한국어 전송을 위한 설정
+	public ResponseEntity<String> uploadAjax(MultipartFile file) {
+		
+		logger.info("OriginalFilename : "+file.getOriginalFilename());
+		logger.info("Size : "+file.getSize());
+		logger.info("ContentType : "+file.getContentType());
+		
+		return 	new ResponseEntity<String>(file.getOriginalFilename(), HttpStatus.CREATED);
 	}
 	
 	private String uploadFile(String originalName, byte[] fileData) throws Exception{
