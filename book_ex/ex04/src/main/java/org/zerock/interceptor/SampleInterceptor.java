02194e2f9ef0d63973c0ddb2598f070fd6eb6786
HK Lee <nikerun23@naver.com>
2017-12-30 23:37:47 +0900

    HandlerInterceptorAdapter 구현 클래스 추가

---
+++ book_ex/ex04/src/main/java/org/zerock/interceptor/SampleInterceptor.java
@@ -0,0 +1,7 @@
+package org.zerock.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SampleInterceptor extends HandlerInterceptorAdapter{
+
+}
