5920bd1135b0725ba1161ed0194003926d1c3cbb
HK Lee <nikerun23@naver.com>
2017-12-31 19:41:22 +0900

    HttpSession에 postHandle을 이용해 객체를 추가하여 전송하기

--- book_ex/ex04/src/main/java/org/zerock/interceptor/SampleInterceptor.java
+++ book_ex/ex04/src/main/java/org/zerock/interceptor/SampleInterceptor.java
@@ -19,6 +19,13 @@ public class SampleInterceptor extends HandlerInterceptorAdapter{
 		
 		System.out.println("post handle................");
 		
+		Object result = modelAndView.getModel().get("result");
+		
+		if(result != null) {
+			request.getSession().setAttribute("result", result);
+			response.sendRedirect("/doA");
+		}
+		
 	}
 	
 	@Override
--- book_ex/ex04/src/main/webapp/WEB-INF/views/home.jsp
+++ book_ex/ex04/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,4 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
 <html>
 <head>
 	<title>Home</title>
@@ -9,6 +8,7 @@
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<h2>${result}</h2>
+
 </body>
 </html>
