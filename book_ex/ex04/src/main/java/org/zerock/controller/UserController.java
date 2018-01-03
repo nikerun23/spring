e1e8385c639bac9008dd8b7347abd88ca0b19058
HK Lee <nikerun23@naver.com>
2018-01-03 23:38:26 +0900

    로그인한 정보를 처리 할 User콘트롤러 추가

---
+++ book_ex/ex04/src/main/java/org/zerock/controller/UserController.java
@@ -0,0 +1,21 @@
+package org.zerock.controller;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.UserVO;
+import org.zerock.dto.LoginDTO;
+import org.zerock.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+	
+}
