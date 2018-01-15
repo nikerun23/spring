9d40ec1f2fce1d2074a23a2df36e12ad9210cae8
HK Lee <nikerun23@naver.com>
2018-01-15 22:56:02 +0900

    로그인한 계정의 sessionKey와 sessionLimit을  업데이트하기 위한 dao 추가

--- book_ex/ex04/src/main/java/org/zerock/persistence/UserDAOImpl.java
+++ book_ex/ex04/src/main/java/org/zerock/persistence/UserDAOImpl.java
@@ -1,5 +1,9 @@
 package org.zerock.persistence;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -21,5 +25,22 @@ public class UserDAOImpl implements UserDAO {
 		return session.selectOne(namespace + ".login", dto);
 	}
 
+	@Override
+	public void keepLogin(String uid, String sessionId, Date next) {
+	
+	Map<String, Object> paramMap = new HashMap<String, Object>();
+	paramMap.put("uid", uid);
+	paramMap.put("sessionId", sessionId);
+	paramMap.put("next", next);
+	
+	session.update(namespace+".keepLogin", paramMap);
+	    
+	}
+	
+	@Override
+	public UserVO checkUserWithSessionKey(String value) {
+	
+	  return session.selectOne(namespace +".checkUserWithSessionKey", value);
+	}
 	
 }
