2f1a07dce793be62ca9229fe22408874cdab6a21
HK Lee <nikerun23@naver.com>
2018-01-20 21:53:47 +0900

    인터셉터에 로그인 쿠키 처리 추가

--- book_ex/ex04/src/main/java/org/zerock/interceptor/AuthInterceptor.java
+++ book_ex/ex04/src/main/java/org/zerock/interceptor/AuthInterceptor.java
@@ -1,5 +1,7 @@
 package org.zerock.interceptor;
 
+import javax.inject.Inject;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -7,27 +9,47 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.WebUtils;
+import org.zerock.domain.UserVO;
+import org.zerock.service.UserService;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
   private static final Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
+
+  @Inject
+  private UserService service;
   
   @Override
   public boolean preHandle(HttpServletRequest request,
       HttpServletResponse response, Object handler) throws Exception {
     
     HttpSession session = request.getSession();   
-
-
-    if(session.getAttribute("login") == null) {
     
-      logger.info("current user is not logined");
-      
-      saveDest(request);
-      
-      response.sendRedirect("/user/login");
-      return false;
-    }
+    if(session.getAttribute("login") == null){
+        
+        logger.info("current user is not logined");
+        
+        saveDest(request);
+        
+        Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+        
+        if(loginCookie != null) { 
+          
+          UserVO userVO = service.checkLoginBefore(loginCookie.getValue());
+          
+          logger.info("USERVO: " + userVO);
+          
+          if(userVO != null){
+            session.setAttribute("login", userVO);
+            return true;
+          }
+          
+        }
+
+        response.sendRedirect("/user/login");
+        return false;
+      }
     return true;
   }
   
