86184cd5c5f5cf7cc51f3b6d148dce90716a48bc
HK Lee <nikerun23@naver.com>
2018-01-25 00:03:28 +0900

    JUnit 단위 테스트

---
+++ book_ex/app1/src/test/java/org/zerock/test/SampleMapperTest.java
@@ -0,0 +1,23 @@
+package org.zerock.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.persistence.SampleMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class SampleMapperTest {
+
+	@Inject
+	private SampleMapper mapper;
+	
+	@Test
+	public void testTime() {
+		System.out.println("------- getName() : "+mapper.getClass().getName());
+		System.out.println("------- getTime() : "+mapper.getTime());
+	}
+}
