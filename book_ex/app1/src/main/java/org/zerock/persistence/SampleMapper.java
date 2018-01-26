b545f6ed37a04647ecf4e924c8cd1bae2316ab0f
HK Lee <nikerun23@naver.com>
2018-01-26 23:10:51 +0900

    @Param 어노테이션을 이용한 다중 파라미터 처리

--- book_ex/app1/src/main/java/org/zerock/persistence/SampleMapper.java
+++ book_ex/app1/src/main/java/org/zerock/persistence/SampleMapper.java
@@ -1,10 +1,14 @@
 package org.zerock.persistence;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface SampleMapper {
 
 	@Select("select now()")
 	public String getTime();
+	
+	@Select("select email from tbl_user where userid = #{id} and userpw = #{pw}")
+	public String getEmail(@Param("id") String id, @Param("pw") String pw);
 	
 }
