0e7270cc91307106017c7237c3fedac6d96316c6
HK Lee <nikerun23@naver.com>
2018-01-26 23:14:53 +0900

    JUnit 테스트 추가

--- book_ex/app1/src/test/java/org/zerock/test/SampleMapperTest.java
+++ book_ex/app1/src/test/java/org/zerock/test/SampleMapperTest.java
@@ -2,6 +2,7 @@ package org.zerock.test;
 
 import javax.inject.Inject;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,9 +16,16 @@ public class SampleMapperTest {
 	@Inject
 	private SampleMapper mapper;
 	
+	@Ignore
 	@Test
 	public void testTime() {
 		System.out.println("------- getName() : "+mapper.getClass().getName());
 		System.out.println("------- getTime() : "+mapper.getTime());
+	}
+	
+	@Test
+	public void testMail() {
+		String email = mapper.getEmail("user00", "user00");
+		System.out.println("------- Email : " + email);
 	}
 }
