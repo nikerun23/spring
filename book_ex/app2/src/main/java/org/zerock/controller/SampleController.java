e63394f0f0c9f2e4c608d65011c3b0cf29f395f8
HK Lee <nikerun23@naver.com>
2018-01-28 19:23:29 +0900

    RestController 추가

---
+++ book_ex/app2/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,16 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		
+		return "Hello World";
+		
+	}
+}
