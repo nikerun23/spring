6900e7b0052f0a4366eca9c7ee675ec6f9a06d77
HK Lee <nikerun23@naver.com>
2018-01-28 19:53:13 +0900

    DataSource 설정 및 테스트

--- book_ex/app2/src/test/java/org/zerock/App2ApplicationTests.java
+++ book_ex/app2/src/test/java/org/zerock/App2ApplicationTests.java
@@ -1,7 +1,13 @@
 package org.zerock;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class App2ApplicationTests {
 
+	@Autowired
+	DataSource ds;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testConnection() throws SQLException {
+		System.out.println("++++++++++++++++++++++");
+		System.out.println(ds);
+		
+		Connection con = ds.getConnection();
+		
+		System.out.println("++++++++++++++++++++++");
+		System.out.println(con);
+		con.close();
 	}
 
 }
