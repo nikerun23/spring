f115c9db97d6ceb9e2acc6aed60a4b2f211224db
HK Lee <nikerun23@naver.com>
2018-01-28 20:21:43 +0900

    @MapperScan 을 이용한 Mapper인터페이스 인식

--- book_ex/app2/src/main/java/org/zerock/App2Application.java
+++ book_ex/app2/src/main/java/org/zerock/App2Application.java
@@ -1,9 +1,11 @@
 package org.zerock;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value= {"org.zerock.mapper"})
 public class App2Application {
 
 	public static void main(String[] args) {
