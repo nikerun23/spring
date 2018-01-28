656163d67e9f649fab3c2f48891dea38da05409c
HK Lee <nikerun23@naver.com>
2018-01-28 19:23:03 +0900

    스프링 부트 프로젝트 생성

---
+++ book_ex/app2/src/main/java/org/zerock/App2Application.java
@@ -0,0 +1,12 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2Application.class, args);
+	}
+}
