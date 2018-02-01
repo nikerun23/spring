525227c2e15252143aa1d9fffa62d425d51a5ed1
HK Lee <nikerun23@naver.com>
2018-02-01 23:28:40 +0900

    테스트 콘트롤러 추가

--- book_ex/app2/src/main/java/org/zerock/controller/HomeController.java
+++ book_ex/app2/src/main/java/org/zerock/controller/HomeController.java
@@ -1,5 +1,15 @@
 package org.zerock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/test")
+	public void test(Model model) {
+		
+		model.addAttribute("msg", "Hello World !!");
+	}
 }
