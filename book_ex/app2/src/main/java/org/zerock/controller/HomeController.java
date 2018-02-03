0eeb7468cdd3e135107d473f033a9cbe50b56a2b
HK Lee <nikerun23@naver.com>
2018-02-03 22:03:47 +0900

    폼의 정보를 받기 위한 POST 콘트롤러 추가

--- book_ex/app2/src/main/java/org/zerock/controller/HomeController.java
+++ book_ex/app2/src/main/java/org/zerock/controller/HomeController.java
@@ -3,13 +3,25 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/test")
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public void test(Model model) {
 		
 		model.addAttribute("msg", "Hello World !!");
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public String testPOST(String username, String address, Model model) {
+		
+		System.out.println("USERNAME : " + username);
+		System.out.println("ADDRESS : " + address);
+		
+		model.addAttribute("msg", "Hello World !!");
+		
+		return "resultSuccess";
 	}
 }
