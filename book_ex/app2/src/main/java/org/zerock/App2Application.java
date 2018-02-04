fc1d8f89186c32de9f28fdb89bcfe33c43e3cb0c
HK Lee <nikerun23@naver.com>
2018-02-04 19:15:59 +0900

    web.xml 에 처리하던 필터 설정을 추가

--- book_ex/app2/src/main/java/org/zerock/App2Application.java
+++ book_ex/app2/src/main/java/org/zerock/App2Application.java
@@ -1,8 +1,14 @@
 package org.zerock;
 
+
+import javax.servlet.Filter;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.zerock.filter.SampleFilter;
 
 @SpringBootApplication
 @MapperScan(value= {"org.zerock.mapper"})
@@ -10,5 +16,18 @@ public class App2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App2Application.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean someFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(getFilter());
+		registration.addUrlPatterns("/test/*");
+		
+		registration.setName("sampleFilter");
+		return registration;
+	}
+	private Filter getFilter() {
+		return new SampleFilter();
 	}
 }
