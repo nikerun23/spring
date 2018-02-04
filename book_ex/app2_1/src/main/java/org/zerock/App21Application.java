a735862c2269bc197da0d4a1935c7b3590d06b23
HK Lee <nikerun23@naver.com>
2018-02-04 19:35:39 +0900

    Thymeleaf 설정을 위한 프로젝트 생성

---
+++ book_ex/app2_1/src/main/java/org/zerock/App21Application.java
@@ -0,0 +1,12 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App21Application.class, args);
+	}
+}
