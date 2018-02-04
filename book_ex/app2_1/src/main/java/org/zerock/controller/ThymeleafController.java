a2e1bb7aa97c06bd2bf0656505edf6c637b3affe
HK Lee <nikerun23@naver.com>
2018-02-04 19:41:53 +0900

    콘트롤러 추가

---
+++ book_ex/app2_1/src/main/java/org/zerock/controller/ThymeleafController.java
@@ -0,0 +1,25 @@
+package org.zerock.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ThymeleafController {
+
+	@RequestMapping("listTest")
+	public void sendData(Model model) {
+		
+		List<String> list = new ArrayList<>();
+		
+		for (int i = 0; i < 20; i++) {
+			list.add("DATA : " + i);
+		}
+		
+		model.addAttribute("name", "Sample Data");
+		model.addAttribute("list", list);
+	}
+}
